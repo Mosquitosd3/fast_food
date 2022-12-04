c00a5d528b921f303d5cb09a9c5ae5171ab9eb19
Mosquito <Mosquitod3@yandex.ru>
2022-12-05 00:14:39 +0300

    Микросервисные системы

--- dish/src/main/java/dsih/DishMainApp.java
+++ dish/src/main/java/dsih/DishMainApp.java
@@ -1,4 +1,11 @@
 package dsih;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DishMainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DishMainApp.class, args);
+    }
 }
